e6a2717081ac83476d0a4cbaa729b3fa59bc6e9c
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:03:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,9 +9,9 @@ end
 def shipping_manifest
   
     the_manifest = {
-      "whale bone corsets" => "5", 
-      "porcelain vases" => "2",
-      "oil paintings" => "3"
+      "whale bone corsets" => 5, 
+      "porcelain vases" => 2,
+      "oil paintings" => 3
     }
 
 end

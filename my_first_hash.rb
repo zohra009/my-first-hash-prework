9b4827bdff7b3ef92f681a237a81dddda16fa57d
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 03:13:54 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,9 +38,9 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun_powder"] = 4
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-  shipping_manifest 
+  shipping_manifest
 end

4a7a36ba654f5ea5e05cabe2c07e2441dc30390f
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 02:43:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = {"money" => "makes the world go round"
+  your_hash = {"money" => "makes the world go round"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
